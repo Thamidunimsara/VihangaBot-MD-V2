f3cb501fbad93847e31b8f3aa32abe16e630ca7f
Thamidunimsara <105425946+Thamidunimsara@users.noreply.github.com>
2022-08-26 18:13:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -24,18 +24,18 @@ global.onlygroup = 'false'
 global.group1 = 'https://chat.whatsapp.com/GJ9bAPxYpktAnqbXVsnRuf' //මෙතන grouplink එක.
 
 //Owner React
-global.OWNER_REACT = '🧑🏻‍💻' //මෙතනට emoji එකක දාන්න.
-global.REACT_NUMBER = '9476342733' //මෙතන number එක.
+global.OWNER_REACT = '🐰💐' //මෙතනට emoji එකක දාන්න.
+global.REACT_NUMBER = '94703044401' //මෙතන number එක.
 
 // Other
-global.owner = ['9476342733',''] //මෙතන number එක.
-global.premium = ['9476342733','9476342733'] //මෙතන number එක.
-global.botname = 'VIHANGA BOT INC' //මෙතන නම.
-global.ownername = 'Vihanga MD' //මෙතන නම.
-global.ownername = 'ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ' //මෙතන නම.
+global.owner = ['94703044401',''] //මෙතන number එක.
+global.premium = ['94703044402','9476342733'] //මෙතන number එක.
+global.botname = '𝙍𝙊𝙒𝘿𝙔 𝘽𝙊𝙏' //මෙතන නම.
+global.ownername = '𝙍𝙊𝙒𝘿𝙔 𝙁𝙁' //මෙතන නම.
+global.ownername = '𝙍𝙊𝙒𝘿𝙔 𝙁𝙁' //මෙතන නම.
 global.group2 = 'https://chat.whatsapp.com/GJ9bAPxYpktAnqbXVsnRuf' //මෙතන grouplink එක.
-global.packname = 'VIHANGA BOT INC' //මෙතන නම.
-global.cap = '*By Vihanga MD*' //මෙතන නම.
+global.packname = '𝙍𝙊𝙒𝘿𝙔 𝙁𝙁' //මෙතන නම.
+global.cap = '*𝘽𝙔 𝙍𝙊𝙒𝘿𝙔 𝙁𝙁*' //මෙතන නම.
 global.img = 'https://telegra.ph/file/88d6fdeff08bbc4945901.jpg' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
 global.ytchannel = 'https://youtu.be/Z9rIR0Vopb8' //මෙතන කැමති ලින්ක් එකක්.
 global.monayawal = '100000' //මෙතන මාරු කරන්න එපා.
@@ -58,11 +58,11 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip' //මෙතන මාරු කරන්න එපා.
 global.f5 = 'application/pdf' //මෙතන මාරු කරන්න එපා.
 global.f6 = 'application/vnd.android.package-archive' //මෙතන මාරු කරන්න එපා.
-global.author = 'VIHANGA BOT INC' //මෙතන නම.
+global.author = '𝙍𝙊𝙒𝘿𝙔 𝙁𝙁' //මෙතන නම.
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=' //මෙතන මාරු කරන්න එපා.
 global.sessionName = 'session' //මෙතන මාරු කරන්න එපා.
 global.prefa = ['','!','.','🐦','🐤','🗿'] //මෙතන මාරු කරන්න එපා.
-global.sp = '⭔' //මෙතන emoji එකක්.
+global.sp = '🐰' //මෙතන emoji එකක්.
 global.mess = {
     success: '*හරි*',
         ban: '*Sorry, looks like u got banned😔😔 type .owner To request unban okay*',
